c38eb8c8476d9a75e70e6b5e1c81a690d70e7982
carlosmoya <eskasector@gmail.com>
2020-03-30 16:10:39 +0200

    Productes

--- servidor/productes.js
+++ servidor/productes.js
@@ -30,11 +30,13 @@ class Obj {
             try {
                 sql = 'CREATE TABLE productes (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, nom VARCHAR(50) NOT NULL, descripcio TEXT, preu INT(6), imatge VARCHAR(255))'
                 await db.promiseQuery(sql)
-                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Tele", "Tele molt xula", 800, "/web/imatges/producte-1.jpg")'
+                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Àfrica Salvatge", "El safari que mai oblidaràs", 1500, "/web/imatges/africa.jpg")'
                 await db.promiseQuery(sql)
-                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Batidora", "Batidora molt xula", 20, "/web/imatges/producte-2.png")'
+                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Santorini", "La ïlla màgica", 1800, "/web/imatges/santorini.jpg")'
                 await db.promiseQuery(sql)
-                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Aspirador", "Aspirador molt xulo", 300, "/web/imatges/producte-3.jpg")'
+                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("La Rivera Maya", "Les vacances de la teva vida", 2600, "/web/imatges/riveramaya.jpg")'
+                await db.promiseQuery(sql)
+                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Reikiavik", "La ciutat gelada", 1700, "/web/imatges/Reikiavik.jpg")'
                 await db.promiseQuery(sql)
             } catch (e) {
                 console.error(e)
@@ -51,7 +53,7 @@ class Obj {
             return result.json({ resultat: "ko", missatge: "Error, funció llistatProductes: ha fallat la crida a les dades"})  
         }   
     
-        // Si hem aconseguit dades corectament, tornem la taula resultant
+        // Si l'usuari existeix i s'ha identificat correctament (amb codi o amb token) retornem 'ok'
         if (typeof taula === 'object' && typeof taula.length === 'number') {
             result.json({ resultat: "ok", missatge: taula })
         } else {
